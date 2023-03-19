c7e71540a1b7ca02b3f555c5faade5131fb88e56
Ray Krueger <raykrueger@gmail.com>
2023-03-19 01:01:32 +0000

    Add the first test

---
+++ test/game-server.test.ts
@@ -0,0 +1,52 @@
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { ContainerImage, AwsLogDriver, Protocol } from 'aws-cdk-lib/aws-ecs';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { GameServer } from '../src';
+
+
+class GameStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new GameServer(this, 'Satisfactory', {
+      cpu: 4096,
+      memoryLimitMiB: 16384,
+      image: ContainerImage.fromRegistry('raykrueger/satisfactory-dedicated-server'),
+      discord: {
+        commandName: 'satisfactory',
+        secretName: 'Discord',
+      },
+      logging: new AwsLogDriver({
+        streamPrefix: 'SatisfactoryLogs',
+        logRetention: RetentionDays.THREE_DAYS,
+      }),
+      dnsConfig: {
+        domainName: 'satisfactory.example.com',
+        hostzedZone: 'xxxxxxxxxxxxxxxxxxxx',
+        assumedRole: 'arn:aws:iam::1234567890:role/assumed-role',
+      },
+      gamePorts: [
+        { portNumber: 7777, protocol: Protocol.UDP },
+        { portNumber: 15000, protocol: Protocol.UDP },
+        { portNumber: 15777, protocol: Protocol.UDP },
+      ],
+      mountTarget: '/home/steam/.config/Epic/FactoryGame/Saved/SaveGames',
+    });
+  }
+}
+
+function buildStack(): cdk.Stack {
+  const app = new cdk.App();
+  return new GameStack(app, 'Test');
+}
+
+
+test('Function with no assumed role', () => {
+  const stack = buildStack();
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::ECS::Cluster', {
+
+  });
+});
