66402287a12f85fcb833f4d2adbcf5a6f968af6f
Ray Krueger <raykrueger@gmail.com>
2024-10-07 02:45:15 +0000

    Use the Lambda arch from the deployment machine
    There seems to be some kind of bug that the 'platform' parameter to the
    code bundling is ignored. When specifying ARM64 as the architecture for
    my lamda, and platform, the lambda bundling (docker buildx) just uses
    the host architecture.

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class Constants {
   static readonly LAMBDA_RUNTIME = Runtime.PYTHON_3_12;
-  static readonly LAMBDA_ARCH = Architecture.ARM_64;
+  static readonly LAMBDA_ARCH = process.arch === 'arm64' ? Architecture.ARM_64 : Architecture.X86_64;
 }
